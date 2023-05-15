d33aae56a5c4e7ebb717ebaeefb9ca6d4182364d
Jiahaun-wsl <cheayuki13@gmail.com>
2023-05-15 13:50:57 +0800

    [fix]type命名,[add]uart的init和操作，暂时没写文档

---
+++ include/kernel/os.h
@@ -0,0 +1,36 @@
+#include <kernel/types.h>
+
+struct context { //上下文寄存器
+	/* ignore x0 */
+	reg_t ra;
+	reg_t sp;
+	reg_t gp;
+	reg_t tp;
+	reg_t t0;
+	reg_t t1;
+	reg_t t2;
+	reg_t s0;
+	reg_t s1;
+	reg_t a0;
+	reg_t a1;
+	reg_t a2;
+	reg_t a3;
+	reg_t a4;
+	reg_t a5;
+	reg_t a6;
+	reg_t a7;
+	reg_t s2;
+	reg_t s3;
+	reg_t s4;
+	reg_t s5;
+	reg_t s6;
+	reg_t s7;
+	reg_t s8;
+	reg_t s9;
+	reg_t s10;
+	reg_t s11;
+	reg_t t3;
+	reg_t t4;
+	reg_t t5;
+	reg_t t6;
+};
--- kernel/boot/init.c
+++ kernel/boot/init.c
@@ -1,45 +1,43 @@
 #include <kernel/types.h>
-// #include <sbi.h>
+#include <kernel/os.h>
+
+extern void uart_init(void);
+extern void uart_puts(char *s);
+
+#define STACK_SIZE 1024
+uint_8 task_stack[STACK_SIZE];
+struct context ctx_task;
 
 volatile int main_counter = 1000;
 
-static inline void init_hard(unit_64 hart_id) {
-    asm volatile("mv tp, %0" : : "r" (hart_id & 0x7));
+static inline void init_hard(uint_64 hart_id)
+{
+    asm volatile("mv tp, %0"
+                 :
+                 : "r"(hart_id & 0x7));
 }
 
+void user_task0(void){
+    while(1){
 
-void start_kernel(unit_64 hart_id) {
+    }
+}
+
+// void init_sched(){
+//     //设置栈指针
+//     ctx_task.sp = (reg_t)&task_stack[STACK_SIZE - 1]; 
+//     //设置任务的第一条指令地址（函数名本身就是地址）
+//     ctx_task.ra = (reg_t)user_task0;
+
+// }
+
+
+void start_kernel(uint_64 hart_id)
+{
     init_hard(hart_id);
+    uart_init();
+	uart_puts("Hello, My first OS!\n");
+    // init_sched();
 
-   while (1) {
-    
-   }
-
-//    if (main_counter == 1000) {
-//        extern unit_64 bsss[];
-//        extern unit_64 bsse[];
-//        for (unit_64 *i = bsss; i < bsse; i++) {
-//            *i = 0;
-//        }
-//        main_counter = main_counter + 1;
-//
-////        console_init();
-// //       pageLock_init();
-//        printf("Hello, risc-v!\nBoot hart_id: %ld \n\n", hart_id);
-
-//        memory_init();
-//        process_init();
-//
-//
-//        for (int i = 1; i < 5; ++ i) {
-//            if (i != hart_id) {
-//                unsigned long mask = 1 << i;
-//                setMode(i);
-//                sbi_send_ipi(&mask);
-//            }
-//        }
-//
-//        trap_init();
-
-
+   
 }
